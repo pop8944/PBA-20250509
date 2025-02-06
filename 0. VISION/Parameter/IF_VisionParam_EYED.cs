cf6e8a066d59f2d8e6c5a376d98009fbfe290068
jhyun <jhyun@intelligentfactory.co.kr>
2025-02-06 17:20:26 +0900

    EYED UI 수정, EYED parameter

---
+++ 0. VISION/Parameter/IF_VisionParam_EYED.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace IntelligentFactory._0._VISION.Parameter
+{
+    public class IF_VisionParam_EYED : IF_VisionParamObject
+    {
+        [CategoryAttribute("Parameter"), DescriptionAttribute(""), DisplayNameAttribute("Model Path")]
+        public string ModelPath { get; set; }
+
+        [CategoryAttribute("Parameter"), DescriptionAttribute(""), DisplayNameAttribute("Threshold Value")]
+        public double Threshold { get; set; } = 0.0;
+
+        [CategoryAttribute("Specification"), DescriptionAttribute(""), DisplayNameAttribute("Rotate Degree Value")]
+        public List<int> RotateDgree { get; set; } = new List<int>{ 0, 90, 180, 270 };
+
+        [CategoryAttribute("Specification"), DescriptionAttribute(""), DisplayNameAttribute("Use Spec Region")]
+        public bool UseSpecRegion { get; set; }
+        public override IF_VisionParamObject Clone()
+        {
+            // 직렬화 시 실제 타입 정보를 포함시키기 위해 TypeNameHandling을 설정
+            string serialize = JsonConvert.SerializeObject(this, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+
+            // 역직렬화 시 TypeNameHandling을 설정
+            IF_VisionParamObject deserialize = JsonConvert.DeserializeObject<IF_VisionParamObject>(serialize, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+
+            return deserialize;
+        }
+    }
+}
